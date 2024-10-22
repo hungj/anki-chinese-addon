8f06043a1de1acc16845cf78f7db7c2a9e3aafea
Jonathan Hung <jhung@jhung-mn2.linkedin.biz>
2024-10-22 09:29:56 -0400

    cleanup

--- addon/__init__.py
+++ addon/__init__.py
@@ -1,16 +1,7 @@
-# import the main window object (mw) from aqt
-from aqt import gui_hooks, mw
-# import the "show info" tool from utils.py
-from aqt.utils import showInfo, qconnect
-# import all of the Qt GUI library
-from aqt.qt import *
+from aqt import gui_hooks
 from anki import notes
-import requests
 import os
 
-
-# We're going to add a menu item below. First we want to create a function to
-# be called when the menu item is activated.
 
 def parse_dict():
     def parse_line(d, line):
@@ -33,8 +24,6 @@ def parse_dict():
             parse_line(d, line)
     return d
 
-d = parse_dict()
-
 def parse_symbols():
     symbols = {}
     dirname = os.path.dirname(__file__)
@@ -47,26 +36,22 @@ def parse_symbols():
                 parts = line.split(',')
                 symbols[parts[0]] = parts[1]
     return symbols
+
+chinese_dict = parse_dict()
 symbols = parse_symbols()
 
 def convert_pinyin(pinyin):
     p = pinyin.split()
     return " ".join([symbols[q] if q in symbols else q for q in p])
 
-def testFunction(changed: bool, note: notes.Note, current_field_idx: int) -> bool:
-    # get the number of cards in the current collection, which is stored in
-    # the main window
-    # cardCount = mw.col.cardCount()
-    # show a message box
-    # print(f"changed: {changed}, current_field_idx: {current_field_idx}")
-    #if changed and note.keys()[current_field_idx].lower() == 'hanzi':
+def autofill(changed: bool, note: notes.Note, current_field_idx: int) -> bool:
     # changed is always False for some reason in anki
     if note.keys()[current_field_idx].lower() == 'hanzi':
         trad = note.values()[current_field_idx].strip()
-        if trad in d:
-            simpl = d[trad]['simplified']
-            pinyin = d[trad]['pinyin']
-            definitions = d[trad]['definitions']
+        if trad in chinese_dict:
+            simpl = chinese_dict[trad]['simplified']
+            pinyin = chinese_dict[trad]['pinyin']
+            definitions = chinese_dict[trad]['definitions']
             print(f'found traditional {trad} with simplified {simpl} and pinyin {pinyin} and definitions {definitions}')
             if not note["Simplified"]:
                 note["Simplified"] = simpl
@@ -74,18 +59,8 @@ def testFunction(changed: bool, note: notes.Note, current_field_idx: int) -> boo
                 note["Pinyin"] = convert_pinyin(pinyin)
             if not note["Meaning"]:
                 note["Meaning"] = ', '.join(definitions)
-            # return true so the note can be reloaded
+            # return True so the note can be reloaded
             return True
     return False
 
-    # for key, value in note.items():
-    #    print(f"for note {note.id}, got key {key} and value {value}")
-
-gui_hooks.editor_did_unfocus_field.append(testFunction)
-
-# create a new menu item, "test"
-#action = QAction("test", mw)
-# set it to call testFunction when it's clicked
-#qconnect(action.triggered, testFunction)
-# and add it to the tools menu
-#mw.form.menuTools.addAction(action)
+gui_hooks.editor_did_unfocus_field.append(autofill)
